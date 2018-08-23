2290192e8079f2106e5c87f6a6a20544f9f90487
Eric Szymanski <EricSzy@dhcp-152-16-15-44.research.duhs.duke.edu>
2018-08-23 13:19:37 -0400

    Lineshape Tool

---
+++ InteractiveLineshape.py
@@ -0,0 +1,110 @@
+from numpy import *
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, CheckButtons, RadioButtons, Button
+from nmrglue import proc_base
+
+fig, ax = plt.subplots(figsize = (7, 7))
+plt.subplots_adjust(left=0.25, bottom=0.25)
+
+pB0 = .05
+wA0, wB0 = 0, 3
+kexAB0 = 1000
+R1a0 = R1b0 = 2.5
+R2a0, R2b0 = 20, 20
+lmf = 150.784627  # 1H for 700MHz spectrometer
+aq = 1.0
+N = 1024 * 4
+T = linspace(0., aq, N)
+dt = aq / N
+xf = arange(-1./(2*dt), 1./(2*dt), 1./dt/N)/lmf
+
+def set_x(lmf):
+	return arange(-1./(2*dt), 1./(2*dt), 1./dt/N)/lmf
+
+def data(lmf, pB, wA, wB, kexAB, R1a, R1b, R2a, R2b):
+	pA = 1 - pB
+	M0 = array([1-pB, pB])
+	k12 = kexAB * pB / (pB + pA)
+	k21 = kexAB * pA / (pB + pA)
+	wA = wA * lmf * 2 * pi
+	wB = wB * lmf * 2 * pi
+	dw = wB - wA
+	dR2 = R2b - R2a
+
+	R = zeros((2, 2), dtype=complex)
+	R[0, 0] = -R2a + 1j*wA - k12
+	R[0, 1] = k21
+	R[1, 0] = k12
+	R[1, 1] = -R2b + 1j*wB - k21
+
+	v,G = linalg.eig(R)
+	G_1 = linalg.inv(G)
+
+	
+	fid = zeros(T.shape, dtype=complex)
+	for i,t in enumerate(T):
+		A = dot(dot(G,diag(exp(v*t))), G_1)
+		fid[i] = sum(dot(A, M0))
+	return proc_base.fft(fid).real
+
+# Inital Plotted Data
+l, = plt.plot(xf, data(lmf, pB0, wA0, wB0, kexAB0, R1a0, R1b0, R2a0, R2b0))
+
+# Initallize Sliders
+axcolor = 'lightgrey'
+ax_pB = plt.axes([0.18, 0.15, 0.3, 0.015], facecolor=axcolor)
+ax_wA = plt.axes([0.18, 0.13, 0.3, 0.015], facecolor=axcolor)
+ax_wB = plt.axes([0.18, 0.11, 0.3, 0.015], facecolor=axcolor)
+ax_R2a = plt.axes([0.60, 0.13, 0.3, 0.015], facecolor = axcolor)
+ax_R2b = plt.axes([0.60, 0.11, 0.3, 0.015], facecolor = axcolor)
+ax_kexAB = plt.axes([0.60, 0.15, 0.3, 0.015], facecolor=axcolor)
+
+# Set slider ID and values
+slider_pB = Slider(ax_pB, 'p$_B$', .01, .5, valinit = pB0)
+slider_wA = Slider(ax_wA, '$\omega$$_A$', -10, 10, valinit = wA0)
+slider_wB = Slider(ax_wB, '$\omega$$_B$', -10, 10, valinit = wB0)
+slider_R2a = Slider(ax_R2a, 'R$_{2a}$', 0, 50, valinit = R2a0)
+slider_R2b = Slider(ax_R2b, 'R$_{2b}$', 0, 50, valinit = R2b0)
+slider_kexAB = Slider(ax_kexAB, 'k$_{ex}$AB', 0, 10000, valinit = kexAB0)
+
+def update(val):
+    pB = slider_pB.val 
+    wA = slider_wA.val 
+    wB = slider_wB.val 
+    R2a = slider_R2a.val
+    R2b = slider_R2b.val
+    kexAB = slider_kexAB.val 
+    
+    l.set_ydata(data(lmf, pB, wA, wB, kexAB, R1a0, R1b0, R2a, R2b))
+    
+    fig.canvas.draw_idle()
+
+slider_pB.on_changed(update)
+slider_wA.on_changed(update)
+slider_wB.on_changed(update)
+slider_R2a.on_changed(update)
+slider_R2b.on_changed(update)
+slider_kexAB.on_changed(update)
+
+rax = plt.axes([0.01, 0.35, 0.15, 0.1])
+radio = RadioButtons(rax, ('Carbon', 'Nitrogen', 'Proton'))
+
+# RadioButtons for lmf
+def changelmf(label):
+    lmfdict = {'Carbon':150.784627, 'Nitrogen':60.76302, 'Proton':600}
+    global lmf
+    lmf = lmfdict[label] 
+    pB = slider_pB.val 
+    wA = slider_wA.val 
+    wB = slider_wB.val 
+    R2a = slider_R2a.val
+    R2b = slider_R2b.val
+    kexAB = slider_kexAB.val 
+
+    l.set_ydata(data(lmf, pB, wA, wB, kexAB, R1a0, R1b0, R2a, R2b))
+    l.set_xdata(set_x(lmf))
+    ax.axis([min(set_x(lmf)), max(set_x(lmf)), None, None])
+    plt.draw()
+radio.on_clicked(changelmf)
+
+plt.show()
